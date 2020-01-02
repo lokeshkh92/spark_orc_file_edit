fbdbe0bbf3259073e88295f1f3f4b8b2dd21ac69
Lokesh Khandelwal <42270097+lokeshkh92@users.noreply.github.com>
2020-01-02 14:32:53 +0530

    Remove redundant code

--- orc_filre_edit.py
+++ orc_filre_edit.py
@@ -5,8 +5,7 @@ from pyspark.sql.types import *
 def filename(path):
     return path
 
-sourceFile = udf(filename, StringType())
-spark = SparkSession.builder.appName("Find FileName").getOrCreate()
+spark = SparkSession.builder.appName("Edit orc file").getOrCreate()
 A = spark.read.orc('<basepath of orc file>')
 B = A.withColumn("<column_name>", regexp_replace("<column_name>", "<value to be replaced>", "<value which is to be replaced>"))
 B.write.partitionBy("<partition column>").format("orc").save('<basepath where orc file to be written>')
