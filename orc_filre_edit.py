e14d56a4efc40d5cce7b4c7909037d62245be9c7
Lokesh Khandelwal <42270097+lokeshkh92@users.noreply.github.com>
2020-06-05 15:19:44 +0530

    Added functionality to replace column from a list
    Added functionality to replace value of column by randomly selecting values from a list

--- orc_filre_edit.py
+++ orc_filre_edit.py
@@ -1,11 +1,25 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+import random
 
 def filename(path):
     return path
 
+#list from which value to be relaced in a column
+pf = ['Forbidden','Non-Preferred','Preferred']
+
+#Create spark session
 spark = SparkSession.builder.appName("Edit orc file").getOrCreate()
+
+#Input path for orc file
 A = spark.read.orc('<basepath of orc file>')
+
+# Replace value of a column with the regex
 B = A.withColumn("<column_name>", regexp_replace("<column_name>", "<value to be replaced>", "<value which is to be replaced>"))
-B.write.partitionBy("<partition column>").format("orc").save('<basepath where orc file to be written>')
+
+# Replace values in a column from a list of values mentioned above
+C = B.withColumn("<column_name>",lit(random.choice(pf)))
+
+#Save the dataframe as orc file alongwith partition column intact
+C.write.partitionBy("<partition column>").format("orc").save('<basepath where orc file to be written>')
